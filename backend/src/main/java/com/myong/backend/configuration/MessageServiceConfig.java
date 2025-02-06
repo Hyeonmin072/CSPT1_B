51fcf7629d9b673f2dc0dda029d26ef1686684e8
rlawjggns <rlawjggns12@gmail.com>
2025-02-06 16:47:54 +0900

    메시지 서비스에서 사용하는 고유 키 보안화

--- backend/src/main/java/com/myong/backend/configuration/MessageServiceConfig.java
+++ backend/src/main/java/com/myong/backend/configuration/MessageServiceConfig.java
@@ -2,14 +2,21 @@ package com.myong.backend.configuration;
 
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.service.DefaultMessageService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MessageServiceConfig {
 
+    @Value("${coolsms.api.key}")
+    private String apiKey;
+
+    @Value("${coolsms.api.secret}")
+    private String apiSecretKey;
+
     @Bean
     DefaultMessageService messageService() {
-        return NurigoApp.INSTANCE.initialize("NCS3TUHASFLVVYWL", "I6VRPL2IO1MWBBWKIHXD9CFW67SZL5R2", "https://api.coolsms.co.kr");
+        return NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, "https://api.coolsms.co.kr");
     }
 }
