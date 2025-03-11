a03e28ab84e4b03d7d474001cc67da1e1f2927f7
rlawjggns <rlawjggns12@gmail.com>
2025-03-11 15:26:40 +0900

    Repository 수정

--- backend/src/main/java/com/myong/backend/repository/ShopRepository.java
+++ backend/src/main/java/com/myong/backend/repository/ShopRepository.java
@@ -2,6 +2,8 @@ package com.myong.backend.repository;
 
 import com.myong.backend.domain.entity.shop.Shop;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,16 +15,10 @@ public interface ShopRepository extends JpaRepository<Shop, UUID> {
 
     Optional<Shop> findByEmail(String email);
 
-
-    @Query("select new com.myong.backend.domain.dto.shop.ShopProfileResponseDto(s.name, s.address, s.post, s.tel, s.pwd, s.desc, s.openTime , s.closeTime, s.regularHoliday)" +
-            "from Shop s " +
-            "where s.email = :email")
-    ShopProfileResponseDto findProfileByEmail(@Param("email")String email);
-
     @Query(value = " select * FROM shop " +
             "Where ST_Distance_Sphere(point(:longitude,:latitude),point(longitude,latitude)) <= 2000",
             nativeQuery = true)
-    List<Shop> findShopWithinRadius(@Param("longitude") double longitude,@Param("latitude") double latitude);
+    List<Shop> findShopWithinRadius(@Param("longitude") double longitude, @Param("latitude") double latitude);
 
     @Query("select s From Shop s where s.address Like :location% ")
     List<Shop> findShopWithAddress(@Param("location") String location);
