f1efb97df24e3ed9268443dfd8c88123d9ca898f
Surnamek <3620123@naver.com>
2025-03-20 17:24:43 +0900

    이력서및 충돌코드 정상화

--- backend/src/main/java/com/myong/backend/service/DesignerService.java
+++ backend/src/main/java/com/myong/backend/service/DesignerService.java
@@ -1,10 +1,14 @@
 package com.myong.backend.service;
 
-import com.myong.backend.domain.dto.designer.SignUpRequest;
-import com.myong.backend.domain.dto.designer.UpdateProfileRequest;
+import com.myong.backend.domain.dto.designer.ResumeRequestDto;
+import com.myong.backend.domain.dto.designer.SignUpRequestDto;
+import com.myong.backend.domain.dto.designer.SignUpRequestDto;
+import com.myong.backend.domain.dto.designer.UpdateProfileRequestDto;
+import com.myong.backend.domain.dto.designer.UpdateProfileRequestDto;
 import com.myong.backend.domain.dto.shop.ShopDesignerDetailResponseDto;
 import com.myong.backend.domain.dto.shop.ShopDesignerRequestDto;
 import com.myong.backend.domain.entity.designer.Designer;
+import com.myong.backend.domain.entity.designer.Resume;
 import com.myong.backend.domain.entity.shop.Shop;
 import com.myong.backend.repository.DesignerRepository;
 import com.myong.backend.repository.ShopRepository;
@@ -27,8 +31,9 @@ public class DesignerService {
     private final DesignerRepository designerRepository;
     private final PasswordEncoder passwordEncoder;
     private final ShopRepository shopRepository;
+    private final ResumeService resumeService;
 
-    public void signUp(SignUpRequest request) {
+    public void signUp(SignUpRequestDto request, ResumeRequestDto resumeRequestDto) {
         // 이메일 중복 체크
         if (checkEmailDuplication(request.getEmail())) {
             throw new IllegalArgumentException("이미 존재하는 이메일입니다.");
@@ -40,6 +45,7 @@ public class DesignerService {
         }
 
         LocalDate birthday;
+
         try {
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
             birthday = LocalDate.parse(request.getBirth(), formatter);
@@ -57,20 +63,25 @@ public class DesignerService {
                 request.getGender()
         );
         designerRepository.save(designer);
+
+        //이력서 생성
+        Resume resume = resumeService.createResume(designer.getEmail());
+        log.info("resume : {}", resume);
     }
 
-    public Designer getProfile(String nickname) {
-        return designerRepository.findByNickName(nickname)
+    //프로필 가져오기
+    public Designer getProfile(String email) {
+        return designerRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("디자이너를 찾을 수 없습니다"));
     }
 
     @Transactional
-    public Designer updateProfile(String nickname, UpdateProfileRequest updateProfileRequest) {
-        Designer designer = designerRepository.findByNickName(nickname)
+    public Designer updateProfile(String email, UpdateProfileRequestDto updateProfileRequest) {
+        Designer designer = designerRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("디자이너를 찾을 수 없습니다"));
 
         // 닉네임 변경 시 중복 체크
-        if (updateProfileRequest.getUpdateNickName() != null && !updateProfileRequest.getUpdateNickName().equals(nickname)) {
+        if (updateProfileRequest.getUpdateNickName() != null && !updateProfileRequest.getUpdateNickName().equals(email)) {
             if (checkNicknameDuplication(updateProfileRequest.getUpdateNickName())) {
                 throw new IllegalArgumentException("이미 존재하는 닉네임입니다.");
             }
@@ -78,10 +89,11 @@ public class DesignerService {
             log.info("updateNickName : {}", updateProfileRequest.getUpdateNickName());
         }
 
-
+        //디자이너 소개 수정
         designer.updateDesc(updateProfileRequest.getUpdateDesc());
         log.info("updateDesc : {}", updateProfileRequest.getUpdateDesc());
 
+        //비밀번호 변경
         if (updateProfileRequest.getNewPwd() != null) {
             if (updateProfileRequest.getOldPwd() == null || !designer.getPwd().equals(updateProfileRequest.getOldPwd())) {
                 throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
@@ -96,11 +108,14 @@ public class DesignerService {
             log.info("updatePwd : {}", updateProfileRequest.getNewPwd());
         }
 
+        //전화번호 변경
         if (updateProfileRequest.getUpdateTel() != null) {
             designer.updateTel(updateProfileRequest.getUpdateTel());
             log.info("updateTel : {}", updateProfileRequest.getUpdateTel());
         }
 
+
+        //이미지 변경
         if (updateProfileRequest.getUpdateImage() != null) {
             designer.updateImage(updateProfileRequest.getUpdateImage());
             log.info("updateImage : {}", updateProfileRequest.getUpdateImage());
@@ -110,14 +125,20 @@ public class DesignerService {
         return designerRepository.save(designer);
     }
 
+    //이메일 중복검사 매서드
     public Boolean checkEmailDuplication(String email) {
         return designerRepository.existsByEmail(email);
     }
 
+    //닉네임 중복검사 매서드
     public Boolean checkNicknameDuplication(String nickName) {
         return designerRepository.existsByNickName(nickName);
     }
 
+    //이력서 불러오기
+    public Resume getResume(String email) {
+        return resumeService.getResume(email);
+    }
     public ShopDesignerDetailResponseDto getDesigner(ShopDesignerRequestDto request) {
         Designer designer = designerRepository.findByEmail(request.getDesignerEmail())
                 .orElseThrow(() -> new NoSuchElementException("해당 디자이너를 찾을 수 없습니다.")); // 디자이너 찾기
