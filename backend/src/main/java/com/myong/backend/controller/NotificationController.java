7c83724531ed4443fb00a1d77a9383d560a9ec89
rlawjggns <rlawjggns12@gmail.com>
2025-05-17 19:53:28 +0900

    SSE 알람 기능 구현 v0.3

--- backend/src/main/java/com/myong/backend/controller/NotificationController.java
+++ backend/src/main/java/com/myong/backend/controller/NotificationController.java
@@ -19,6 +19,8 @@ public class NotificationController {
     private final NotificationService notificationService;
 
     // Last-Event-ID는 SSE 연결이 끊어진 경우, 클라이언트가 수신한 마지막 데이터의 ID를 의미한다. 항상 존재하는 것이 아니므로 false
+    // SSE 연결이 끊겼을 때 재연결하는 과정에서 브라우저가 자동으로 마지막 이벤트 ID를 포함해서 서버에 요청을 보낸다
+    // 한 번 이 API를 호출해두면, 브라우저가 자동으로 재연결을 반복해준다.
     @GetMapping(value = "/connect", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public ResponseEntity<SseEmitter> connect(@RequestHeader(value = "Last-Event-ID", required = false, defaultValue = "") String lastEventId,
                                               @AuthenticationPrincipal UserDetailsDto userDetailsDto) {
--- backend/src/main/java/com/myong/backend/service/NotificationService.java
+++ backend/src/main/java/com/myong/backend/service/NotificationService.java
@@ -1,6 +1,5 @@
 package com.myong.backend.service;
 
-import com.myong.backend.domain.dto.NotificationSendResponseDto;
 import com.myong.backend.domain.entity.Notification;
 import com.myong.backend.repository.EmitterRepository;
 import com.myong.backend.repository.NotificationRepository;
@@ -21,62 +20,71 @@ public class NotificationService {
     private final EmitterRepository  emitterRepository;
     private final NotificationRepository notificationRepository;
 
-    // 연결 지속 시간 -> 1시간
+    // 연결 지속 시간 -> 1시간  -> 1시간 동안 아무 이벤트도 보내지 않으면 타임아웃
+    // 중간에 알람이라도 하나 보내면 타임아웃이 갱신된다
     private static final Long DEFAULT_TIMEOUT = 60L * 1000 * 60;
 
     public SseEmitter connect(String userEmail, String lastEventId) {
         // 매 연결마다 고유한 이벤트 아이디 생성
+        // 이 값은 클라이언트가 수신한 마지막 이벤트를 기억할 수 있게 하기 위해 사용
+        // 이 ID는 클라이언트가 자동 재연결 시 Last-Event-ID로 되돌려 보내주는 기준이 된다.
         String eventId = userEmail + "_" + System.currentTimeMillis();
         
         // SseEmitter 인스턴스 생성 후 Map에 저장
+        // 연결된 클라이언트 1명에 대한 SSE 스트림 객체
+        // 이 emitter를 통해 메시지를 계속 밀어넣는다.
+        // sendToClient()는 결국 이 emitter 인스턴스의 .send()를 호출한다
         SseEmitter emitter = emitterRepository.save(eventId, new SseEmitter(DEFAULT_TIMEOUT));
 
-        // 이벤트 전송 시, 자동으로 삭제
+        // 클라이언트가 연결을 정상적으로 종료했다면, SseEmitter 인스턴스를 자동으로 삭제
         emitter.onCompletion(() -> {
             log.info("onCompletion 콜백");
             emitterRepository.deleteById(eventId);
         });
         
-        // 이벤트 전송 시, 시간 초과가 되면 자동으로 삭제
+        // SseEmitter가 설정된 시간 안에 아무 이벤트도 전송하지 않았으면, SseEmitter 인스턴스를 자동으로 삭제
         emitter.onTimeout(() -> {
             log.info("onTimeout 콜백");
             emitterRepository.deleteById(eventId);
-                    
         });
 
         // 최초 연결 시 응답을 보내지 않으면 503 오류가 발생 -> 오류 방지를 위해 최초 연결시 더미 이벤트 생성 후 클라이언트에 전송
         sendToClient(eventId, emitter, "알림 서버 연결 성공. [userEmail=" + userEmail + "]");
 
-        // 클라이언트가 미수신한 이벤트 목록이 존재할 경우 -> 모두 전송하여 이벤트 유실 예방
-        // Last-Event-Id가 있으면 여기서 사용된다 -> 전송되지 않고 쌓인 이벤트들을 찾아 보내기 위해 사용
+        // [재전송 로직]
+        // 클라이언트가 마지막으로 받은 이벤트 ID(Last-Event-ID)를 서버에 보내면,
+        // 그 이후에 서버에서 발생한 이벤트들만 필터링하여 재전송함.
+        // 클라이언트와의 연결 끊긴 사이에 놓친 알림들을 복구하는 역할
         if(!lastEventId.isEmpty()) {
             Map<String, Object> events = emitterRepository.findAllEventCacheStartWithByuserEmail(userEmail);
             events.entrySet().stream()
+                    // a.compareTo(b) 가 음수 -> a 가 b 보다 작다는 뜻 -> lastEventId보다 큰 것들만 필터링
                     .filter(e -> lastEventId.compareTo(e.getKey()) < 0)
                     .forEach(e -> sendToClient(e.getKey(), emitter, e.getValue()));
         }
-
+        // 최초 연결 요청에 대해 서버가 SSE 스트림을 열어주면서 SseEmitter를 반환
+        // 클라이언트는 이 반환된 SseEmitter를 통해 서버로부터 이벤트를 받게 된다.
         return emitter;
     }
 
     /**
-     * 다른 서비스 클래스에서 이벤트 발생 시 이 메서드를 호출하여 알림을 보낸다
+     * 다른 서비스 클래스에서 이벤트 발생 시, 이 메서드를 호출하여 SSE 연결을 통해 알림을 보낸다
      * @param notification 알람 객체(이 객체를 DB에 저장한 후 넘어올 것으로 기대)
      */
     public void send(Notification notification) {
         String eventId = notification.getUser().getEmail() + "_" + System.currentTimeMillis();
 
-        // 유저의 SseEmitter 모둑 가져오기
+        // 한 사용자가 여러 클라이언트(브라우저 탭, 모바일 앱 등)에서 접속할 수 있으므로, 사용자의 모든 SSE 연결(emitter)을 가져와 알림을 전송한다.
         Map<String, Object> sseEmiters = emitterRepository.findAllEventCacheStartWithByuserEmail(notification.getUser().getEmail());
         sseEmiters.forEach(
                 (key, emitter) -> {
                     try {
-                        // 데이터 캐시 저장(유실된 데이터 처리하기 위함)
+                        // 전송 전 알림 데이터를 캐시에 저장하여 네트워크 장애 등으로 인한 이벤트 유실을 방지한다.
+                        // 전송 시점에서 실패하여 현재 emitter가 삭제되어도, 클라이언트가 재접속할 때 Last-Event-ID를 이용해 캐시에서 못 받은 알림들을 다시 보낼 수 있다.
                         emitterRepository.saveEventCache(key, notification);
-                        // 데이터 전송
-                        sendToClient(eventId, (SseEmitter) emitter, new NotificationSendResponseDto(eventId, notification.getContent()));
+                        sendToClient(eventId, (SseEmitter) emitter, notification.getContent());
                     } catch (Exception e) {
-                        emitterRepository.deleteById(key); // 실패한 emitter 제거
+                        emitterRepository.deleteById(key); // 전송 중 오류 발생 시 해당 emitter는 삭제 -> 서버는 더 이상 그 연결을 통해 이벤트를 보내지 않는다
                         log.error("알람 전송 실패", e);
                     }
                 }
@@ -85,16 +93,18 @@ public class NotificationService {
     }
 
     /**
-     * 서버 측 이벤트 -> 클라이언트로 직접 보내는 메서드
+     * 서버 측 이벤트를 클라이언트로 전송 메서드
      * @param eventId 이벤트 아이디
      * @param emitter SseEmitter 객체
      * @param object 데이터
      */
     private void sendToClient(String eventId, SseEmitter emitter, Object object) {
         try {
+            // SseEmitter의 send() 메서드를 통해 클라이언트에 이벤트 전송, 이름은 "connect"로 지정
+            // 이벤트 ID와 데이터를 함께 전송하여 클라이언트가 수신 이벤트를 추적할 수 있게 한다.
             emitter.send(SseEmitter.event().name("connect").id(eventId).data(object));
         } catch (IOException e) {
-            emitterRepository.deleteById(eventId);
+            emitterRepository.deleteById(eventId); // 예외 발생 시, 해당 emitter를 삭제하여 더 이상 이벤트를 보내지 않도록 한다
             throw new RuntimeException("알림 서버 연결 오류");
         }
     }
