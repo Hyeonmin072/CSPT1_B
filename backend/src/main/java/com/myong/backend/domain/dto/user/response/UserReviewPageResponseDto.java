99618a27f0a7afc8cd182a9d0239b5ef78a5c8b5
hyeongseopE <gudtjq363636@naver.com>
2025-07-10 14:18:37 +0900

    [fix]:이름 수정일, 답변, 메뉴카테고리

--- backend/src/main/java/com/myong/backend/domain/dto/user/response/UserReviewPageResponseDto.java
+++ backend/src/main/java/com/myong/backend/domain/dto/user/response/UserReviewPageResponseDto.java
@@ -1,11 +1,14 @@
 package com.myong.backend.domain.dto.user.response;
 
 
+import com.myong.backend.domain.entity.shop.MenuCategory;
 import com.myong.backend.domain.entity.usershop.Review;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
@@ -17,5 +20,9 @@ public class UserReviewPageResponseDto {
     private String shopName;
     private String designerName;
     private String designerEmail;
+    private String menuName;
+    private LocalDateTime createdAt;
+    private String reply;
+    private MenuCategory category;
 
 }
--- backend/src/main/java/com/myong/backend/repository/ReviewRepository.java
+++ backend/src/main/java/com/myong/backend/repository/ReviewRepository.java
@@ -55,8 +55,8 @@ public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<ReviewData> findAllByDesignerEmail(@Param("designerEmail") String email);
 
     @Query("select new com.myong.backend.domain.dto.user.response.UserReviewPageResponseDto(" +
-            "r.rating, r.content, r.image, s.email, s.name, d.name, d.email) " +
-            "from Review r join r.shop s join r.designer d " +
+            "r.rating, r.content, r.image, s.email, s.name, d.name, d.email, re.menu.name, r.createDate, ra.content, re.menu.category) " +
+            "from Review r join r.shop s join r.designer d join r.reservation re join r.reviewAnswer ra " +
             "where r.user = :user " +
             "order by r.createDate Desc")
     List<UserReviewPageResponseDto> findByUserOrderByCreateDateDesc(@Param("user") User user, Pageable pageable);
